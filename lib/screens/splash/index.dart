8d8caaed679bd3d9804ccd1063ea2d70e8ea7460
newit-huytt <huytt@newit.co.jp>
2021-09-19 14:11:49 +0700

    style: add splash screen

--- lib/screens/splash/index.dart
+++ lib/screens/splash/index.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:photo_app/widget/button/elevated_button.dart';
+import 'package:photo_app/widget/button/outlined_button.dart';
 
 class SplashScreen extends StatelessWidget {
   static const PAGE_NAME = '/';
@@ -7,13 +9,56 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text(
-          AppLocalizations.of(context)!.language,
+    final size = MediaQuery.of(context).size;
+
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        Image.asset(
+          'assets/images/splash_background.jpeg',
+          fit: BoxFit.cover,
+          height: size.height,
+          width: size.width,
         ),
-      ),
+        Image.asset(
+          'assets/images/logo.png',
+          height: size.height/10,
+          width: size.width/2,
+          fit: BoxFit.contain,
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: CustomOutlinedButton(
+                      label: (AppLocalizations.of(context)!.log_in).toUpperCase(),
+                      onTap: () {
+                        // TODO 
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  Flexible(
+                    flex: 1,
+                    child: CustomElevatedButton(
+                      label: (AppLocalizations.of(context)!.register).toUpperCase(),
+                      onTap: () {
+                        // TODO 
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
