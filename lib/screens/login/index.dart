7dfc9613c441b98877ff6dab6fc6139f3eb74a98
newit-huytt <huytt@newit.co.jp>
2021-09-20 11:36:50 +0700

    style: responsive login/register page

--- lib/screens/login/index.dart
+++ lib/screens/login/index.dart
@@ -5,6 +5,7 @@ import 'package:photo_app/screens/discover/index.dart';
 import 'package:photo_app/widget/button/elevated_button.dart';
 import 'package:photo_app/widget/floating_button.dart';
 import 'package:photo_app/widget/input_field_decoration.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class LoginScreen extends StatelessWidget {
   static const PAGE_NAME = '/login';
@@ -30,53 +31,60 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           physics: const ClampingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(AppLocalizations.of(context)!.log_in,
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                
-                // Username
-                const SizedBox(height: 32),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  maxLines: 1,
-                  cursorColor: Theme.of(context).primaryColor,
-                  decoration: InputFieldDecoration.inputDecoration(
-                    context: context,
-                    hint: AppLocalizations.of(context)!.user_name,
+          child: Center(
+            child: Container(
+              width: getValueForScreenType<double>(
+                context: context,
+                mobile: double.infinity,
+                tablet: MediaQuery.of(context).size.width/2,
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(AppLocalizations.of(context)!.log_in,
+                    style: Theme.of(context).textTheme.headline1,
                   ),
-                  onChanged: (value) => {},
-                ),
-        
-                // Password
-                const SizedBox(height: 16),
-                TextField(
-                  keyboardType: TextInputType.text,
-                  maxLines: 1,
-                  obscureText: true,
-                  cursorColor: Theme.of(context).primaryColor,
-                  decoration: InputFieldDecoration.inputDecoration(
-                    context: context,
-                    hint: AppLocalizations.of(context)!.password,
+                  
+                  // Username
+                  const SizedBox(height: 32),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    maxLines: 1,
+                    cursorColor: Theme.of(context).primaryColor,
+                    decoration: InputFieldDecoration.inputDecoration(
+                      context: context,
+                      hint: AppLocalizations.of(context)!.user_name,
+                    ),
+                    onChanged: (value) => {},
                   ),
-                  onChanged: (value) => {},
-                ),
         
-                // Login button
-                const SizedBox(height: 16),
-                CustomElevatedButton(
-                  label: (AppLocalizations.of(context)!.log_in).toUpperCase(),
-                  onTap: () {
-                    Navigator.of(context).pushNamed(DiscoverScreen.PAGE_NAME);
-                  },
-                ),
-              ],
+                  // Password
+                  const SizedBox(height: 16),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    maxLines: 1,
+                    obscureText: true,
+                    cursorColor: Theme.of(context).primaryColor,
+                    decoration: InputFieldDecoration.inputDecoration(
+                      context: context,
+                      hint: AppLocalizations.of(context)!.password,
+                    ),
+                    onChanged: (value) => {},
+                  ),
+        
+                  // Login button
+                  const SizedBox(height: 16),
+                  CustomElevatedButton(
+                    label: (AppLocalizations.of(context)!.log_in).toUpperCase(),
+                    onTap: () {
+                      Navigator.of(context).pushNamed(DiscoverScreen.PAGE_NAME);
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/register/index.dart
+++ lib/screens/register/index.dart
@@ -5,6 +5,7 @@ import 'package:photo_app/screens/discover/index.dart';
 import 'package:photo_app/widget/button/elevated_button.dart';
 import 'package:photo_app/widget/floating_button.dart';
 import 'package:photo_app/widget/input_field_decoration.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class RegisterScreen extends StatelessWidget {
   static const PAGE_NAME = '/register';
@@ -30,73 +31,80 @@ class RegisterScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           physics: const ClampingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(AppLocalizations.of(context)!.register,
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                
-                // Username
-                const SizedBox(height: 32),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  maxLines: 1,
-                  cursorColor: Theme.of(context).primaryColor,
-                  decoration: InputFieldDecoration.inputDecoration(
-                    context: context,
-                    hint: AppLocalizations.of(context)!.user_name,
+          child: Center(
+            child: Container(
+              width: getValueForScreenType<double>(
+                context: context,
+                mobile: double.infinity,
+                tablet: MediaQuery.of(context).size.width/2,
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(AppLocalizations.of(context)!.register,
+                    style: Theme.of(context).textTheme.headline1,
                   ),
-                  onChanged: (value) => {},
-                ),
-            
-                // Password
-                const SizedBox(height: 16),
-                TextField(
-                  keyboardType: TextInputType.text,
-                  maxLines: 1,
-                  obscureText: true,
-                  cursorColor: Theme.of(context).primaryColor,
-                  decoration: InputFieldDecoration.inputDecoration(
-                    context: context,
-                    hint: AppLocalizations.of(context)!.password,
+                  
+                  // Username
+                  const SizedBox(height: 32),
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    maxLines: 1,
+                    cursorColor: Theme.of(context).primaryColor,
+                    decoration: InputFieldDecoration.inputDecoration(
+                      context: context,
+                      hint: AppLocalizations.of(context)!.user_name,
+                    ),
+                    onChanged: (value) => {},
                   ),
-                  onChanged: (value) => {},
-                ),
-            
-                // Confirm Password
-                const SizedBox(height: 16),
-                TextField(
-                  keyboardType: TextInputType.text,
-                  maxLines: 1,
-                  obscureText: true,
-                  cursorColor: Theme.of(context).primaryColor,
-                  decoration: InputFieldDecoration.inputDecoration(
-                    context: context,
-                    hint: AppLocalizations.of(context)!.confirm_password,
+              
+                  // Password
+                  const SizedBox(height: 16),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    maxLines: 1,
+                    obscureText: true,
+                    cursorColor: Theme.of(context).primaryColor,
+                    decoration: InputFieldDecoration.inputDecoration(
+                      context: context,
+                      hint: AppLocalizations.of(context)!.password,
+                    ),
+                    onChanged: (value) => {},
                   ),
-                  onChanged: (value) => {},
-                ),
-            
-                // Sign up button
-                const SizedBox(height: 16),
-                CustomElevatedButton(
-                  label: (AppLocalizations.of(context)!.sign_up).toUpperCase(),
-                  onTap: () {
-                    Navigator.of(context).pushNamed(DiscoverScreen.PAGE_NAME);
-                  },
-                ),
-            
-                // Condition
-                const SizedBox(height: 32),
-                Text(AppLocalizations.of(context)!.condition_on_register,
-                  style: Theme.of(context).textTheme.caption,
-                )
-              ],
+              
+                  // Confirm Password
+                  const SizedBox(height: 16),
+                  TextField(
+                    keyboardType: TextInputType.text,
+                    maxLines: 1,
+                    obscureText: true,
+                    cursorColor: Theme.of(context).primaryColor,
+                    decoration: InputFieldDecoration.inputDecoration(
+                      context: context,
+                      hint: AppLocalizations.of(context)!.confirm_password,
+                    ),
+                    onChanged: (value) => {},
+                  ),
+              
+                  // Sign up button
+                  const SizedBox(height: 16),
+                  CustomElevatedButton(
+                    label: (AppLocalizations.of(context)!.sign_up).toUpperCase(),
+                    onTap: () {
+                      Navigator.of(context).pushNamed(DiscoverScreen.PAGE_NAME);
+                    },
+                  ),
+              
+                  // Condition
+                  const SizedBox(height: 32),
+                  Text(AppLocalizations.of(context)!.condition_on_register,
+                    style: Theme.of(context).textTheme.caption,
+                  )
+                ],
+              ),
             ),
           ),
         ),
